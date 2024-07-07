9b22f33043b606ff461f843a9a06ab897aea0f71
Pongnapat Limmongkolhirun <pongnapatlimmongkolhirun@gmail.com>
2024-07-07 15:46:28 +0700

    navbar change

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ import { MdEmail } from "react-icons/md";
 
 const Navbar = () =>{
     return(
-        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 text-gray-300 z-20">
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 text-gray-300 z-20 bg-black">
             <div>
                 <Link to="name" smooth duration={500} className="font-semibold cursor-pointer text-transparent bg-clip-text bg-gradient-to-r hover:from-[#005aa7] from-white to-white hover:to-[#fffde4]">
                     Pongnapat L
@@ -26,16 +26,22 @@ const Navbar = () =>{
             </ul>
             <div className="hidden lg:flex fixed flex-col right-0 top-[35%]">
                 <ul>
-                    <li className="font-bold flex w-[160px] h-[60px] items-center mr-[-100px] hover:mr-[10px] duration-500">
-                        <FaGithub size={35}/>&emsp; Github
+                    <li className="font-bold flex w-[160px] h-[60px] items-center mr-[-100px] hover:mr-[10px] duration-500 justify-between">
+                        <a href="https://github.com/bookxeng" className="flex items-center w-full">
+                            <FaGithub size={35}/>
+                            <span className="ml-6">Github</span>
+                        </a>
                     </li>
-                    <li className="font-bold flex w-[160px] h-[60px] items-center mr-[-100px] hover:mr-[10px] duration-500">
-                        <MdEmail size={35}/>&emsp; E-mail
+                    <li className="font-bold flex w-[160px] h-[60px] items-center mr-[-100px] hover:mr-[10px] duration-500 justify-between">
+                        <a href="mailto:pongnapatlimmongkolhirun@gmail.com" className="flex items-center w-full">
+                            <MdEmail size={35}/>
+                            <span className="ml-6">Email</span>
+                        </a>
                     </li>
                 </ul>
             </div>
         </div>
-    )
+    );
 }
 
 export default Navbar;
