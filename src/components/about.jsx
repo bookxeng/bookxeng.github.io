211a18c85d885791780aa5635cc21d0be66b3038
Pongnapat L <pongnapatlimmongkolhirun@gmail.com>
2025-01-28 13:19:11 +0700

    change desc

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,7 +5,7 @@ const About = () =>{
         <div name ="about" className="w-full min-h-screen bg-black">
             <div className="max-w-[1000px] mx-auto px-12 flex flex-col items-center justify-center h-full text-gray-50">
                 <h1 className="font-extrabold text-5xl py-4">Bookxeng</h1>
-                <p className="font-semibold">&emsp;Valorant pro player radiant rank, Apex predator, Overwatch top 500, CSGO faceit Level10, RB6 Champion rank, Father of FPS Game</p>
+                <p className="font-semibold">&emsp;Pongnapat</p>
             </div>
         </div>
     )
